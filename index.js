640c66bee3da1e92f76323bf7ce992d07dff4903
Barbara-D <bdeskar@gmail.com>
2022-06-13 12:45:59 +0200

    remove gitignore, push dist

---
+++ index.js
@@ -0,0 +1,7 @@
+let $0102d920810385a3$var$selint;
+$0102d920810385a3$var$selint = 3;
+let $0102d920810385a3$var$dev = 3;
+console.log($0102d920810385a3$var$selint, $0102d920810385a3$var$dev);
+
+
+//# sourceMappingURL=index.js.map
