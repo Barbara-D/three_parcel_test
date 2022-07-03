152317161c204b9ad8fda10e21d8e41c58cee219
Barbara-D <bdeskar@gmail.com>
2022-07-03 10:14:55 +0200

    create basic three.js elements

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # three_parcel_test
+
 Test run of using parcel to deploy a three.js project on gh pages
+
+//RUN THE LOCAL SERVER
+npm run start
+
+//DEPLOY TO GH PAGES
+npm run deploy
+
+//GH PAGE LINK
+https://barbara-d.github.io/three_parcel_test/
--- src/script.js
+++ src/script.js
@@ -1,7 +1,45 @@
-let selint;
+import "./style.css";
+import * as THREE from "three";
 
-selint = 3;
+console.log(THREE);
 
-let dev = 3;
+//Scene
+const scene = new THREE.Scene();
 
-console.log(selint, dev);
+//Blue cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// //Sizes
+// const sizes = {
+//   width: 800,
+//   height: 600,
+// };
+
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+scene.add(camera);
+camera.position.z = 3;
+camera.position.y = 1.2;
+
+// Set up lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+dirLight.position.set(0, 20, 5); // x, y, z
+scene.add(dirLight);
+
+//Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
